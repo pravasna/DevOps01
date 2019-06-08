8bfeaf72001482f350cd68e1957bf2a669e041e6
Prasanna <pravasna@gmail.com>
2019-06-08 02:36:28 +0000

    Added part02 commands

---
+++ devops01a.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Display free disk space
+df -h
+
+# Print # of CPUs
+
+nproc
+
+cat /proc/cpuinfo
+
+# Ram
+
+free -m
+
+# IP Address
+curl ifconfig.me
+
+# MAC Address (NOTE This will work only on MAC), 
+ifconfig -a
+ # For Mac - networksetup -listallhardwareports 
